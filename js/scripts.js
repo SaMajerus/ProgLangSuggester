f4f186283145b3a1765adf422cd0222ff461cedc
Samuel Majerus <ladolego@gmail.com>
2022-08-12 10:28:01 -0700

    [WIP: Quiz questions, event-handling and -listening logic]

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//Business Logic 
+
+
+
+
+
+//User Interface (UI) Logic  
+function handleQzAns(event){  //Handles user's submitted answers 
+  event.preventDefault();   //Prevents default behavior of 'omsubmit' event from occurring (which would prevent the page from functioning as intended). 
+
+  //Retrieves user's answers, saves them. 
+  const a1 = document.getElementById("ans1").value; 
+  const a2 = document.getElementById("ans2").value; 
+  const a3 = document.getElementById("ans3").value; 
+  const a4 = document.getElementById("ans4").value; 
+  const a5 = document.getElementById("ans5").value; 
+
+
+} 
+
