05d936c5c326177db6f663fb69b4d737bd6f7a71
Samuel Majerus <ladolego@gmail.com>
2022-08-12 11:06:12 -0700

    WIP: Quiz questions, answer-selection UI (input form types)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 //Business Logic 
+function evalAnswers (a1, a2, a3, a4, a5){  //Determines what user's Ans-combination is, and returns a language suggestion based on it. 
 
+
+
+}
 
 
 
@@ -9,11 +13,11 @@ function handleQzAns(event){  //Handles user's submitted answers
   event.preventDefault();   //Prevents default behavior of 'omsubmit' event from occurring (which would prevent the page from functioning as intended). 
 
   //Retrieves user's answers, saves them. 
-  const a1 = document.getElementById("ans1").value; 
-  const a2 = document.getElementById("ans2").value; 
-  const a3 = document.getElementById("ans3").value; 
-  const a4 = document.getElementById("ans4").value; 
-  const a5 = document.getElementById("ans5").value; 
+  const one = document.getElementById("ans1").value; 
+  const two = document.getElementById("ans2").value; 
+  const three = document.getElementById("ans3").value; 
+  const four = document.getElementById("ans4").value; 
+  const five = document.getElementById("ans5").value; 
 
 
 } 
