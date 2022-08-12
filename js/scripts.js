d22a95c5a5a62505d6ba0f9635c3c900afc1387d
Samuel Majerus <ladolego@gmail.com>
2022-08-12 16:32:04 -0700

    All back-end functions and logic completed and working. Ditto front-end and UI.   WIP: CSS Styling, README.

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ function evalAnswers(a1, a2, a3, a4, a5){  //Determines what best suggestion is 
   } else if(a1 === 4) {
     swift+=1;  //Increment by 1
   } else if(a1 === 5) {  
-    //Increment all four by 1 
+    //Increment all by 1 
     sharp+=1;  
     java+=1;  
     pyth+=1;  
@@ -39,13 +39,13 @@ function evalAnswers(a1, a2, a3, a4, a5){  //Determines what best suggestion is 
   } else if(a2 === 2) {
     pyth+=1;  
   } else if(a2 === 3) {
-    //Increment all four by 1 
+    //Increment all by 1 
     sharp+=1;  
     java+=1;  
     pyth+=1;  
     swift+=1;  
   } else if(a2 === 4) { 
-    //Increment all except Python var. 
+    //Increment all except Py. 
     sharp+=1; 
     java+=1; 
     swift+=1; 
@@ -55,18 +55,13 @@ function evalAnswers(a1, a2, a3, a4, a5){  //Determines what best suggestion is 
 
   //Question 3 
   if(a3 === 1) {
-    pyth+=1;  //Increment by 1
+    java+=2;  //Increment by 2
   } else if(a3 === 2) {
     java+=1;  //Increment by 1
   } else if(a3 === 3) {
-    sharp+=1;  //Increment by 1
-  } else if(a3 === 4) {
-    swift+=1;  //Increment by 1
-  } else if(a3 === 5) {  
-    //Increment all four by 1 
-    sharp+=1;  
-    java+=1;  
-    pyth+=1;  
+    //Increment all except JS. 
+    sharp+=1; 
+    pyth+=1; 
     swift+=1; 
   }  //Else: increment nothing 
 
@@ -74,44 +69,62 @@ function evalAnswers(a1, a2, a3, a4, a5){  //Determines what best suggestion is 
 
   //Question 4 
   if(a4 === 1) {
-    pyth+=1;  //Increment by 1
-  } else if(a1 === 2) {
-    java+=1;  //Increment by 1
-  } else if(a1 === 3) {
-    sharp+=1;  //Increment by 1
-  } else if(a1 === 4) {
-    swift+=1;  //Increment by 1
-  } else if(a1 === 5) {  
-    //Increment all four by 1 
+    //Increment all by 1 
     sharp+=1;  
     java+=1;  
     pyth+=1;  
     swift+=1; 
+  } else if(a4 === 2) {
+    java+=2;  //Increment by 1
+  } else if(a4 === 3) {
+    //Increment JS by 2, everything else by 1 
+    sharp+=1; 
+    java+=2; 
+    pyth+=1;  
+    swift+=1; 
+  } else if(a4 === 4) {
+    //Increment JS and C# by 1
+    java+=1; 
+    sharp+=1; 
+  } else if(a4 === 5) {  
+    //Increment Py by 3   
+    pyth+=3; 
+  } else if(a4 === 6) { 
+    //Increment everything by 2, except Swift (increment that by 1) 
+    sharp+=2; 
+    java+=2; 
+    pyth+=2; 
+    swift+=1; 
+  } else if(a4 === 7) { 
+    //Increment JS by 3, everything else by 2 
+    sharp+=2; 
+    java+=3; 
+    pyth+=2; 
+    swift+=2; 
+  } else if(a4 === 8) {
+    //Increment all by 3 
+    sharp+=3; 
+    java+=3; 
+    pyth+=3; 
+    swift+=3; 
   }  //Else: increment nothing 
 
 
 
   //Question 5 
   if(a5 === 1) {
-    pyth+=1;  //Increment by 1
-  } else if(a1 === 2) {
-    java+=1;  //Increment by 1
-  } else if(a1 === 3) {
-    sharp+=1;  //Increment by 1
-  } else if(a1 === 4) {
-    swift+=1;  //Increment by 1
-  } else if(a1 === 5) {  
-    //Increment all four by 1 
-    sharp+=1;  
-    java+=1;  
-    pyth+=1;  
+    //Increment all by 1 
+    sharp+=1; 
+    java+=1; 
+    pyth+=1; 
     swift+=1; 
   }  //Else: increment nothing 
 
 
-
-  return evalRatings(sharp, java, pyth, swift);
+  return (evalRatings(sharp, java, pyth, swift));
 }
+
+
 
 function evalRatings(r1, r2, r3, r4){  //Accepts the 'rating variables' from 'evalAnswers' function as parameters 
   let best; 
@@ -127,7 +140,6 @@ function evalRatings(r1, r2, r3, r4){  //Accepts the 'rating variables' from 'ev
   } else {   //IF-and-only-if [IFF]  two or more Rating variables are equal in value (and greater than the others in value). 
     best = "It was a tie between at least two of the languages. Take the quiz again?"  /*  Won't do logic that will give the User a more specific answer [yet], because 
     the Code Review's requirements forbid us from suggesting two or more Programming Languages to the user.  */  
-
   }
 
   return best;
@@ -152,13 +164,15 @@ function handleQzAns(event){  //Handles user's submitted answers
   const five = parseInt(document.getElementById("ans5").value);  
 
   /*Next line commented out temporarily until the function is finished.*/ 
-  //result = evalAnswers(one, two, three, four, five);   
+  result = evalAnswers(one, two, three, four, five);   
 
   document.getElementById("suggestion").innerText = result; 
 } 
+
 
 
 window.addEventListener("load", function(){
   const quiz = document.getElementById("Qs"); 
   quiz.addEventListener("submit", handleQzAns);
 }); 
+
